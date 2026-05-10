10cbbb62e78ed1080fd42f184106b6f1deec1d6c
newkrishna983-lang <newkrishna983@gmail.com>
2026-05-10 19:33:03 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,7 +4,7 @@ from os import environ
 # API Configuration
 API_ID = int(os.environ.get("API_ID", "34422904"))
 API_HASH = os.environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8131948332:AAGSnLmbACkg7dNOgfqBXdfDBMg-3qzIxyE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8335828145:AAFRbPYc5oZhckHxxGLYM4KiEP2DFurMwhw")
 
 CREDIT = os.environ.get("CREDIT", "•Ɱའ★Krΐຮhήส•❤️‍🔥")
 # MongoDB Configuration
